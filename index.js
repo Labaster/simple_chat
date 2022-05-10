8166d228625556973da4967a43b04b407c817213
labaster <defjamatl@gmail.com>
2022-05-10 12:00:23 +0300

    new commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import Koa from 'koa';
+import cors from '@koa/cors';
+import render from 'koa-ejs';
+import serve from 'koa-static';
+import mount from 'koa-mount';
+import { Server as SocketIo } from 'socket.io';
+import path from 'path';
+import router from './routers/index.js';
+
+const app = new Koa();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
+
+app
+  .use(cors())
+  .use(router.allowedMethods())
+  .use(router.routes())
+  .use(mount('/', serve(__dirname + '/public')));
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'index',
+  viewExt: 'ejs',
+});
+
+const server = app.listen(PORT, () => console.log(`Server started at port http://localhost:${PORT}`));
+const io = new SocketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected!')
+
+  socket.username = "Anonymous"
+  socket.on('change_username', (data) => socket.username = data.username);
+
+  socket.on('new_message', (data) => {
+    io.sockets.emit('add_mess', { message: data.message, username: socket.username, className: data.className });
+  })
+
+  socket.on('typing', (data) => {
+    console.log(data);
+    socket.broadcast.emit('typing', { username: socket.username })
+  })
+});
+
+process.on('uncaughtException', (err) => {
+  console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
+  console.error(err.stack);
+  process.exit(1);
+});
---
+++ public/chat.js
@@ -0,0 +1,70 @@
+$(() => {
+  const socket = io.connect("http://localhost:3000");
+
+  const message = $("#message");
+  const username = $("#username");
+  const send_message = $("#send_message");
+  const send_username = $("#send_username");
+  const chatroom = $("#chatroom");
+  const feedback = $("#feedback");
+
+  send_message.click(() => {
+    if (message.val()) {
+      socket.emit("new_message", {
+        message: message.val(),
+        className: alertClass
+      });
+    }
+  });
+  const min = 1;
+  const max = 6;
+  const random = Math.floor(Math.random() * (max - min)) + min;
+
+  let alertClass;
+  switch (random) {
+    case 1:
+      alertClass = "secondary";
+      break;
+    case 2:
+      alertClass = "danger";
+      break;
+    case 3:
+      alertClass = "success";
+      break;
+    case 4:
+      alertClass = "warning";
+      break;
+    case 5:
+      alertClass = "info";
+      break;
+    case 6:
+      alertClass = "light";
+      break;
+  }
+
+  socket.on("add_mess", data => {
+    feedback.html("");
+    message.val("");
+    chatroom.append(
+      "<div class='alert alert-" +
+        data.className +
+        "'<b>" +
+        data.username +
+        "</b>: " +
+        data.message +
+        "</div>"
+    );
+  });
+
+  send_username.click(() => {
+    socket.emit("change_username", { username: username.val() });
+  });
+
+  message.bind("keypress", () => socket.emit("typing"));
+
+  socket.on("typing", data => {
+    feedback.html(
+      "<p><i>" + data.username + " is writing a message..." + "</i></p>"
+    );
+  });
+});
