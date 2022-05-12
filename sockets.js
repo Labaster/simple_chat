61c5300f30a636e3e82ac8c65a94189374fb6499
labaster <defjamatl@gmail.com>
2022-05-12 15:38:35 +0300

    add storing users is

--- sockets.js
+++ sockets.js
@@ -3,10 +3,13 @@ import nickName from 'generate-username';
 import _ from 'lodash';
 import { chatModel } from './models/chat.js';
 
+const clientsMap = new Map();;
+
 export default (server) => {
   const io = new SocketIo(server);
 
   return io.on('connection', (socket) => {
+    console.log('socket id', socket.id);
     socket.username = _.get(socket, 'handshake.auth.token', '');
     if (!socket.username) {
       const randName = `Anonym_${nickName.default()}`;
@@ -16,9 +19,13 @@ export default (server) => {
       console.log(`We set new rand username: ${randName}!`);
     }
 
+    clientsMap.set(socket.username, socket.id);
+
     socket.on('change_username', (data) => {
+      clientsMap.delete(socket.username);
       socket.username = data.username;
       socket.handshake.auth.token = data.username;
+      clientsMap.set(socket.username, socket.id);
     });
   
     socket.on('new_message', (data) => {
@@ -30,13 +37,22 @@ export default (server) => {
       };
 
       chatModel.saveMsg(dataObj)
-        .then((result) => console.log('msg saved!'))
+        .then(() => console.log('msg saved!'))
         .catch(err => console.log('chatModel.saveMsg -->', err));
       
+      /** Broadcast to all */
       io.sockets.emit('add_mess', dataObj);
+
+      /** send to some one */
+      // io.to(clientsMap.get('Anonym_YouRedstone')).emit('add_mess', dataObj);
     });
   
     socket.on('typing', () => socket.broadcast.emit('typing', { username: socket.username }));
+
+    socket.on('disconnect', () => {
+      console.log('disconnect client event....');
+      clientsMap.delete(socket.username);
+    });
 
     socket.on('connect_error', (err) => {
       console.log(`connect_error due to ${err.message}`);
